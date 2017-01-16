71971d327e3758fa2e006ab472da6ae00365cc60
AberDerBart <nonatz@web.de>
2017-01-16 12:35:45 +0100

    implemented default values for config parameters

--- Config.py
+++ Config.py
@@ -3,10 +3,10 @@ import configparser
 class Config:
 	parser=configparser.ConfigParser()
 	parser.read("mpdScheduler.ini")
-	host=parser.get("general","host")
-	port=parser.getint("general","port")
+	host=parser.get("general","host",fallback="localhost")
+	port=parser.getint("general","port",fallback=6600)
 
-	alarmDefaultSong=parser.get("alarm","defaultSong")
-	alarmFadeTime=parser.getint("alarm","fadeTime")
+	alarmDefaultSong=parser.get("alarm","defaultSong",fallback=None)
+	alarmFadeTime=parser.getint("alarm","fadeTime",fallback=60)
 
-	sleepFadeTime=parser.getint("sleep","fadeTime")
+	sleepFadeTime=parser.getint("sleep","fadeTime",fallback=60)
